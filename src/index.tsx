ee61e6580d212b79d6988232067df2d9d677edcb
Thibault “Adædra” Hamel <contact@adaedra.eu>
2019-01-17 16:34:39 +0100

    Cleanup
    * Added prepare step
    * Moved initializer to its own file
    * index file is now a correct index, exporting public functions

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,3 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { start as startWebsocket } from './websocket'
-import Selector from './selector'
-
-type Config = {
-    [x: string]: JSX.Element
-}
-
-export function initialize(configGenerator: () => Config) {
-    document.addEventListener('DOMContentLoaded', () => {
-        const config = configGenerator()
-
-        const rootNode = document.createElement('div')
-        rootNode.classList.add('sb-root')
-
-        document.body.appendChild(rootNode)
-        render(<Selector boards={config} />, rootNode)
-
-        startWebsocket()
-    })
-}
+export { default as initialize } from './initialize'
+export { default as dispatcher } from './websocket'
+export { default as Widget } from './widget'
---
+++ src/initialize.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { start as startWebsocket } from './websocket'
+import Selector from './selector'
+
+type Config = {
+    [x: string]: JSX.Element
+}
+
+export default function initialize(configGenerator: () => Config) {
+    document.addEventListener('DOMContentLoaded', () => {
+        const config = configGenerator()
+
+        const rootNode = document.createElement('div')
+        rootNode.classList.add('sb-root')
+
+        document.body.appendChild(rootNode)
+        render(<Selector boards={config} />, rootNode)
+
+        startWebsocket()
+    })
+}
