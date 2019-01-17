5fd168687a46a0502053bda001ddd8c17195c142
Thibault “Adædra” Hamel <contact@adaedra.eu>
2019-01-17 20:42:49 +0100

    More flexible subscribing

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,12 +1,20 @@
-import { BehaviorSubject } from 'rxjs'
+import { BehaviorSubject, Observable } from 'rxjs'
+import { pluck, map } from 'rxjs/operators'
 import { webSocket } from 'rxjs/webSocket'
 
-let dispatcher = new BehaviorSubject({})
+let dispatcher = new BehaviorSubject<{ [key: string]: any }>({})
 
 export function start() {
     webSocket('ws://localhost:1337').subscribe(dispatcher)
 }
 
 dispatcher.subscribe(value => console.log('dispatcher', value))
+
+export function subscribeTo<T>(key: string): Observable<{ [key: string]: T }> {
+    return dispatcher.pipe(
+        pluck(key),
+        map((value: T) => ({ [key]: value }))
+    )
+}
 
 export default dispatcher
--- src/widget.tsx
+++ src/widget.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import { Observable, Subscription } from 'rxjs'
-import { map } from 'lodash'
 
 abstract class Widget<P, S> extends React.Component<P, S> {
-    sources: { [name: string]: Observable<any> }
-    subscriptions: Subscription[] = []
+    subscription?: Subscription
 
     constructor(props) {
         super(props)
         this.state = {} as any
     }
 
+    subscribe(): Observable<S> | undefined {
+        return undefined
+    }
+
     componentDidMount() {
-        this.subscriptions = map(this.sources, (observable, key) =>
-            observable.subscribe(value =>
-                this.setState({ [key]: value } as any)
-            )
-        )
+        const obs = this.subscribe()
+        if (obs) {
+            this.subscription = obs.subscribe(value => this.setState(value))
+        }
     }
 
     componentWillUnmount() {
-        this.subscriptions.forEach(subscription => subscription.unsubscribe())
-        this.subscriptions = []
+        if (this.subscription) {
+            this.subscription.unsubscribe()
+            this.subscription = undefined
+        }
     }
 }
 
